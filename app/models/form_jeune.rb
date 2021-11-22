f19d4a55d2c357ad3013273288c54f8f89127cc7
brian.mtr@outlook.fr <brian.mtr@outlook.fr>
2021-11-22 11:56:26 +0100

    erros message to form_jeunes

--- app/models/form_jeune.rb
+++ app/models/form_jeune.rb
@@ -2,5 +2,7 @@ class FormJeune < ApplicationRecord
     has_many :user_jeunes
     has_many :users, through: :user_jeunes
 
+    validates :image_data, presence: { message: 'Vous devez charger un CV' }
+
     include ImageUploader::Attachment.new(:image)
 end
